4a0e6cfbf326dd67b45431fef16fa41d34eea6f5
pravik2891 <pravik2891@gmail.com>
2020-08-04 13:17:38 +0530

    changes done newly

--- src/org/git2/Gittest2.java
+++ src/org/git2/Gittest2.java
@@ -2,9 +2,18 @@ package org.git2;
 
 public class Gittest2 {
 	
-	public void method() {
+	public void usingLogic() {
+		String name = "pravin";
+		char[] namearray = name.toCharArray();
+		System.out.println(namearray);
 		
-	}
+		String reversed = "";
+		for(int i=namearray.length-1;i>=0;i--) {
+			reversed = reversed+namearray[i];
+			
+		}
+		System.out.println(reversed);
+		}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
